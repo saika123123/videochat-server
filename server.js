17b1ef48df855651a51793f4ed8939d6cc349e69
a-saika <saika1154ok@gmail.com>
2024-07-23 00:59:56 +0900

    bbb

--- server.js
+++ server.js
@@ -53,3 +53,11 @@ app.get('/api/meetings', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const cors = require('cors');
+app.use(cors());
+
+app.use((req, res, next) => {
+    console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+    next();
+  });
